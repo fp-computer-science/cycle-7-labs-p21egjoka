f528350023620ad4b0627268479396f47eee866d
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2020-11-04 09:12:54 -0500

    Lab 2-1
    Virtual Work

---
+++ lab2-1.py
@@ -0,0 +1,16 @@
+# Author: Emanuel Mark Gjoka (AMDG) 11/4/2020
+
+print("Fairfield " + "Prep")
+
+print("-" * 20)
+
+a = "I'm loving this short story I'm reading,"
+b = ' "The  Fall of the House of Usher." '
+
+iii = a + b
+print(iii)
+
+print(len(iii))
+
+vi = "apple"
+print(vi[3])
