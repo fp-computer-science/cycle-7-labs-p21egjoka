62542f4a300d52890211d370ecd9226859284c09
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2020-11-05 11:38:12 -0500

    Lab 3-1
    Physical Work

---
+++ lab_3-1.py
@@ -0,0 +1,11 @@
+# Author: Emanuel Mark Gjoka (AMDG) 11/5/2020
+
+school = "Fairfield Prep"
+
+first_half = school[: 9]
+second_half = school[9:]
+
+print(first_half)
+print(second_half)
+
+print(first_half + second_half)
