0fbb8f9f8a886d479bc62b4daedc801dc46667cc
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2020-11-06 13:07:02 -0500

    Lab 4
    Virtual Work

---
+++ lab_4-1.py
@@ -0,0 +1,9 @@
+# Author: Emanuel Mark Gjoka (AMDG) 11/6/2020
+
+word = "flibbertigibbet"
+
+print(word.find("t"))
+
+phrase = "I wish, I wish, I was a fish."
+
+print(phrase.split(","))
---
+++ lab_4-2.py
@@ -0,0 +1,7 @@
+# Author: Emanuel Mark Gjoka (AMDG) 11/5/2020
+
+name_i = input("What is your name?")
+name_ii = input("What is their name?")
+greetings = "Hello {0}. My name is {1}.".format(name_ii, name_i)
+
+print(greetings)
